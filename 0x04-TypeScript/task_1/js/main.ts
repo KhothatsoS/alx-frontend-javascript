0c44ce8b776409e4d79b699d034c313d5c42f0b1
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-09 12:07:42 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,7 +35,7 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher {firstName, lastName} {
+function printTeacher (firstName: string, lastName: string): string {
   return `${firstName}. ${lastName}`;
 }
 
