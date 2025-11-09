9b36a1821f4f1f8a0b16dc62d672ed5180c61987
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-09 20:54:41 +0200

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -6,7 +6,7 @@ import * as CRUD from './crud.js';
 const row: RowElement = {
   firstName: "Khothatso",
   lastName: "Sibasa",
-  age: 23,
+  age: 23
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
@@ -14,7 +14,7 @@ const newRowID: RowID = CRUD.insertRow(row);
 const updatedRow: RowElement = {
   firstName: "Khothatso",
   lastName: "Sibasa",
-  ...row, age: 23,
+  ...row, age: 23
 };
 
 CRUD.updateRow(newRowID, updatedRow);
