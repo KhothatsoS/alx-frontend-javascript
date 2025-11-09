49c621764b9901d53050f3d79c6c83d15f3f5281
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-09 12:28:59 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -32,16 +32,15 @@ const director1: Director = {
 console.log(director1);
 
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+    (firstName: string, lastName: string): string;
+}
+function printTeacher(firstName: string, lastName: string) {
+    // return `${firstName.charAt(0)}. ${lastName}`;
+    return `${firstName}. ${lastName}`;
 }
 
-function printTeacher{firstName,lastName}
-function printTeacher(firstName: string, lastName: string) {
-  return `${firstName}. ${lastName}`;
-  return `${firstName.charAt(0)}.${lastName}`
-}
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+// const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+//     return `${firstName.charAt(0)}. ${lastName}`;
+// };
 
 console.log(printTeacher("Last", "Sibanyoni"));
