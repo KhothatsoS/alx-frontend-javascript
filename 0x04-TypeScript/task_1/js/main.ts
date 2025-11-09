0b57965a947ba0138796d15bd63054b70e07eef3
KhothatsoS <khothatsosibasa@gmail.com>
2025-11-09 12:17:41 +0200

    Create main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,42 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  location: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'Last',
+  fullTimeEmployee: false,
+  lastName: 'Sibanyoni',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+interface Director extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Director = {
+  firstName: "Last",
+  lastName: "Sibanyoni",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher (firstName, lastName): string{
+  return `${firstName}. ${lastName}`;
+}
+
+console.log(printTeacher("Last", "Sibanyoni"));
